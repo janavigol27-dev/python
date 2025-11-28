3f83ed1a253f4da4143a0484c480c0c1ae2f8253
janvi <janavigol27@gmail.com>
2025-11-28 12:58:59 +0530

    file

--- project8.py
+++ project8.py
@@ -1,228 +1,251 @@
-# print("Welcome to our Numpy Analyzer !")
-# print("="*28)
-
-# while True : 
-#     print("Choose an option : ")
-#     print("1.Creat a Numpy Array")
-#     print("2.Perform Mathematical Operations")
-#     print("3.Combine orSplit Arrays")
-#     print("4.Search, Sort, or Filter Arrays")
-#     print("5.Compute Aggregates and Statistics")
-#     print("6.Exit \n")
-
-#     choice=int(input("Enter your choice : "))
-#     print()
-
-#     if choice==1:
-#         while True:
-#             print("Select the type of array to areate : ")
-#             print("1.1D Array")
-#             print("1.2D Array")
-#             print("1.3D Array")
-#             choice=int(input("Enter your choice : "))
-#             print()
-
-#             if choice==1:
-
-
-                
-
-
-
-
-
-
-
 import numpy as np
-
-class DataAnalyzer:
-    arr = None
-
-    def array(self):
-        print("Array Creation : ")
-        print("1.1D Array")
-        print("1.2D Array")
-        print("1.3D Array")
+class DataAnalytics:
+    arr=None
+    
+    def create_array(self):
+        print("Select the type of array to create : ")
+        print("1. 1D Array")
+        print("2. 2D Array")
+        print("3. 3D Array\n")
+        
         choice=int(input("Enter your choice : "))
-
-        try:
-            if choice==1:
-                count=list(map(int(input("Enter elements (space separated) : ").split())))
-                DataAnalyzer.arr=np.array(count)
-
-                while True:
-                    print("Choose an option : ")
-                    print("1.Indexing")
-                    print("2.Slicing")
-                    print("3.Go Back")
-                    choice=int(input("Enter your choice : "))
-                    print()
-                    if choice==1:
-                        if DataAnalyzer.arr is None:
-                            print("No array created yet ! ")
-                            return
-                        else:
-                            index=tuple(map(int(input("Enter the index (ex: 1 , or 1 2 , or 0 1 2) : ").split())))
-                            print("Index value :",DataAnalyzer.arr[index])
-
-                    elif choice==2:
-                        if DataAnalyzer.arr is None:
-                            print("No array created yet ! ")
-                            return
-                        else: 
-                            print("Enter slicing in this format: end (for 1D) : ")
-                            slice=input("Enter slice range : ")
-                            result=eval(f"DataAnalytics.arr[{slice}]")
-                            print("Sliced output : ",result)
-                    elif choice==3:
-                        break
-
-                    else:
-                        print("Invalid choice !")
-
-            elif choice==2:
-                row=int(input("Enter rows : "))
-                column=int(input("Enter columns : "))
-                count=list(map(int(input("Enter elements (space separated) : ").split())))
-                DataAnalyzer.arr=np.array(count).reshape(row,column)
-
-                while True:
-                    print("Choose an option : ")
-                    print("1.Indexing")
-                    print("2.Slicing")
-                    print("3.Go Back")
-                    choice=int(input("Enter your choice : "))
-                    print()
-                    if choice==1:
-                        if DataAnalyzer.arr is None:
-                            print("No array created yet ! ")
-                            return
-                        else:
-                            r=int(input("Enter row index : "))
-                            c=int(input("Enter column index : "))
-                            print("Index value : ",DataAnalyzer.arr[r,c])
-
-                    elif choice == 2:
-                        if DataAnalyzer.arr is None:
-                            print("No array created yet ! ")
-                            return
-                        else:
-                            r1=map(int(input("Enter the row range (start:end) : ").split()))
-                            c1=map(int(input("Enter the column range (start:end) : ").split()))
-                            print("Sliced Array : ",DataAnalyzer.arr[r1,c1])
-                    elif choice==3:
-                        break
-
-                    else:
-                        print("Invalid choice !")
-            
-            elif choice==3:
-                abc=map(int(input("Enter dimensions(x y z) : ").split()))
-                count=list(map(int, input("Enter elements (space separated): ").split()))
-                DataAnalyzer.arr=np.array(count).reshape(abc)
-
-            else:
-                print("Invalid choice ! ")
-                return
-            print("Array created :\n ",DataAnalyzer.arr)
-        except Exception as e:
-            print("Error creating Array : ",e)
-
-    def math(self):
-        if DataAnalyzer.arr is None:
-            print("No array created yet ! ")
+        if choice==1:
+            element=list(map(int, input("Enter element (space separated) : ").split()))
+            self.array = np.array(element)
+            DataAnalytics.arr = self.array
+            print("Array created successfully.\n", self.array)
+                                            
+        elif choice==2:
+            row = int(input("Enter the number of rows : "))
+            col = int(input("Enter the number of columns : "))
+            total = row * col
+            elements = list(map(int, input(f"Enter {total} elements for the array separated by space:").split()))
+            self.array = np.array(elements).reshape(row, col)
+            DataAnalytics.arr = self.array
+            print("Array Created Successfully.\n", self.array)
+                
+        elif choice==3:
+            x = int(input("Enter dimension 1 (x): "))
+            y = int(input("Enter dimension 2 (y): "))
+            z = int(input("Enter dimension 3 (z): "))
+            total = x * y * z
+            elements = list(map(int, input(f"Enter {total} elements for the 3D array separated by space : ").split()))
+            self.array = np.array(elements).reshape(x, y, z)
+            DataAnalytics.arr = self.array
+            print("Array Created Successfully.\n",self.array)
+                    
+        else:
+            print("Invalid choice!")
             return
-        print("Mathematical Operations : ")
-        print("1. Addition")
+    
+    def math(self):
+        print("Choose a Mathematical Operetions : ")
+        print("1. Addition")  
         print("2. Subtraction")
         print("3. Multiplication")
-        print("4.Division")
-
-        first=list(map(int(input("Enter same-size array elements ( space separated) : " ).split())))
-        convert=np.array(first)
-
-        print("Origional Array : ",DataAnalyzer.arr)
-        print("Second Array : ",convert)
-
-        choice=int(input("Enter your choice : "))
+        print("4. Division\n")
         
-        if choice==1:
-            print("Result of addition :",DataAnalyzer.arr+convert)
-           
-        elif choice==2:
-            print("Result of subtraction :",DataAnalyzer.arr+convert)
+        choice=int(input("Enter your choice : "))
+        if choice == 1:
+            total = DataAnalytics.arr.size
+            elements2 = list(map(int, input(f"Enter {total} elements for second array:").split()))
+            print("Original Array :")
+            print(DataAnalytics.arr)
+            arr2 = np.array(elements2).reshape(DataAnalytics.arr.shape)
+            print("Second Array :")
+            print(arr2)
+            result = DataAnalytics.arr + arr2
+            print("Result of Addition :\n",result)
 
-        elif choice==3:
-            print("Result of multiplication :",DataAnalyzer.arr+convert)
+        elif choice == 2:
+            total = DataAnalytics.arr.size
+            elements2 = list(map(int, input(f"Enter {total} elements for second array:").split()))
+            print("Original Array :")
+            print(DataAnalytics.arr)
+            arr2 = np.array(elements2).reshape(DataAnalytics.arr.shape)
+            print("Second Array :")
+            print(arr2)
+            result = DataAnalytics.arr - arr2
+            print("Result of subtraction : \n",result)
 
-        elif choice==4:
-            print("Result of division :",DataAnalyzer.arr+convert)
+        elif choice == 3:
+            total = DataAnalytics.arr.size
+            elements2 = list(map(int, input(f"Enter {total} elements for second array:").split()))
+            print("Original Array :")
+            print(DataAnalytics.arr)
+            arr2 = np.array(elements2).reshape(DataAnalytics.arr.shape)
+            print("Second Array :")
+            print(arr2)
+            result = DataAnalytics.arr * arr2
+            print("Result of multiplication : \n",result)
 
-        else:
-            print("Invalid choice !")
-
-    def combine_arrays(self):
-        if DataAnalyzer.arr is None:
-            print("No array created yet ! ")
-            return
-        print("Choose an options : ")
-        print("1. Combine Arrays")
-        print("2. Split Arrays")
-
-        ch = int(input("Enter your choice : "))
-
-        if ch == 1:
-            sec=list(map(int(input("Enter the elements of another array to combine (separated by space) : " ).split())))
-            thi=np.array(sec)
-            arr1 = np.array(list(map(int, input("Enter 1st 1D array: ").split())))
-            arr2 = np.array(list(map(int, input("Enter 2nd 1D array: ").split())))
-            print("Combined Array :", np.concatenate((arr1, arr2)))
-
-        elif ch == 2:
-            r = int(input("Enter rows : "))
-            c = int(input("Enter columns : "))
-            arr1 = np.array(list(map(int, input("Enter 1st array elements: ").split()))).reshape(r, c)
-            arr2 = np.array(list(map(int, input("Enter 2nd array elements: ").split()))).reshape(r, c)
-
-            print("How to combine?")
-            print("1. Row-wise (axis=0)")
-            print("2. Column-wise (axis=1)")
-            sub = int(input("Enter your choice : "))
-
-            if sub == 1:
-                print("Combined Array :\n", np.concatenate((arr1, arr2), axis=0))
-            else:
-                print("Combined Array :\n", np.concatenate((arr1, arr2), axis=1))
+        elif choice == 4:
+            total = DataAnalytics.arr.size
+            elements2 = list(map(int, input(f"Enter {total} elements for second array:").split()))
+            print("Original Array :")
+            print(DataAnalytics.arr)
+            arr2 = np.array(elements2).reshape(DataAnalytics.arr.shape)
+            print("Second Array :")
+            print(arr2)
+            result = DataAnalytics.arr / arr2
+            print("Result of division : \n ",result)
 
         else:
             print("Invalid choice!")
+            return
+            
+    def combin(self):
+        print("Choose an option: ")
+        print("1. Combine Array")
+        print("2. Split Array")
 
-    def split_array(self):
-        print("Split Array")
-        arr = np.array(list(map(int, input("Enter array elements: ").split())))
-        n = int(input("Enter number of equal parts : "))
+        choice = int(input("Enter your choice : "))
+        if choice==1:
+            total = DataAnalytics.arr.size
+            elements2 = list(map(int, input(f"Enter {total} elements for second array:").split()))
+            print("Original Array :")
+            print(DataAnalytics.arr)
+            arr2 = np.array(elements2).reshape(DataAnalytics.arr.shape)
+            print("Second Array :")
+            print(arr2)
+            print("combine array (vertical atack) : ")
+            combined = np.concatenate((DataAnalytics.arr, arr2))
+            print("Concatenated Array : \n", combined)
 
-        print("Splitted Arrays :", np.array_split(arr, n))
+        elif choice==2:
+            total = DataAnalytics.arr.size
+            elements2 = list(map(int, input(f"Enter {total} elements for second array:").split()))
+            parts=int(input("Enter numbers of parts to split array : "))
+            print("Original Array :")
+            print(DataAnalytics.arr)
+            arr2 = np.array(elements2).reshape(DataAnalytics.arr.shape)
+            print("\nSecond Array :")
+            print(arr2)
+            print("split array : ")
+            result = np.split(DataAnalytics.arr, parts)
+            print("splitted Array : \n",result )
+        else:
+            print("Invalid choice!")
+            return
+        
+    def search(self):
+        print("Choose an option : ")
+        print("1. Search a value")
+        print("2. Sort the array")
+        print("3. Filter values")
+        
+        choice=int(input("Enter your choice: "))
+        print()
+        if choice==1:
+            print("Original Array:")
+            print(DataAnalytics.arr)
+            print()
+            val = int(input("Enter value to search: "))
+            result = np.where(DataAnalytics.arr == val)
+            print("Found at Index : \n", result)
+                
+        elif choice == 2:
+            while True:
+                print("1.Ascending ordered")
+                print("2.Descending ordered \n")
 
+                choice=int(input("Enter your choice : "))
+                    
+                if choice==1:
+                    print("Original Array:")
+                    print(DataAnalytics.arr)
+                    sorted= np.sort(DataAnalytics.arr)
+                    print("Ascendig Sorted Array :\n ", sorted)
+                    print()
+                    return
 
+                elif choice==2:
+                    print("Original Array:")
+                    print(DataAnalytics.arr)
+                    sorted= np.sort(DataAnalytics.arr)[::-1]
+                    print("Descending Sorted Array : \n", sorted)
+                    print()
+                    return
+                        
+                else:
+                    print("Invalid choice !")
+                    return
 
-print("Welcome to our Numpy Analyzer !")
-print("="*28)
-
-obj=DataAnalyzer() 
-
-while True : 
-    print("Choose an option : ")
-    print("1.Creat a Numpy Array")
-    print("2.Perform Mathematical Operations")
-    print("3.Combine orSplit Arrays")
-    print("4.Search, Sort, or Filter Arrays")
-    print("5.Compute Aggregates and Statistics")
-    print("6.Exit \n")
-
+        elif choice == 3:
+            x = int(input("Enter value to filter greater than: "))
+            filtered= DataAnalytics.arr[DataAnalytics.arr > x]
+            print("Filtered Array : \n", filtered)
+                
+        else:
+            print("Invalid choice!")
+            return
+             
+    def aggre(self):
+        print("Choose an aggregate/statistical operation : ")
+        print("1. Sum")
+        print("2. Mean")
+        print("3. Median")
+        print("4. Standard Deviation")
+        print("5. Variance")
+        
+        choice=int(input("Enter your choice : "))
+        
+        print("Original Array :", DataAnalytics.arr)
+        if choice==1:
+            print("Sum :", np.sum(DataAnalytics.arr))
+                
+        elif choice == 2:
+            print("Mean :", np.mean(DataAnalytics.arr))
+                
+        elif choice == 3:
+            print("Median :", np.median(DataAnalytics.arr))
+            
+        elif choice == 4:
+            print("Standard Deviation :", np.std(DataAnalytics.arr))
+                
+        elif choice == 5:
+            print("Variance :", np.var(DataAnalytics.arr))
+                
+        else:
+            print("Invalid choice!")
+            return
+        
+obj = DataAnalytics()        
+while True:
+    print()
+    print("Welcome to the Numpy Analyzer!")
+    print("="*28)
+    print("Choose an option : \n ")
+        
+    print("1. Create a Numpy Array")
+    print("2. Perform Mathematical Operations")
+    print("3. Combine or Split Arrays")
+    print("4. Search, Sort, or Filter Arrays")
+    print("5. Compute Aggregates and Statistics")
+    print("6. Exit")
+        
     choice=int(input("Enter your choice : "))
     print()
+        
+    if choice == 1:
+        obj.create_array()
 
-    if choice==1:
-        # obj=DataAnalyzer()
-        obj.array()
+    elif choice == 2:
+        obj.math()
+               
+    elif choice==3:
+        obj.combin()
+ 
+    elif choice==4:
+        obj.search()
+
+    elif choice==5:
+        obj.aggre()  
+        
+    elif choice==6:
+        print("Exit")
+        break
+    
+    else:
+        print("Your choice is not valid!")
+        break
